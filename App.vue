4571f8802262fec6e8be7eff77e327e3ac118536
Dust0047 <72190587+Dust0047@users.noreply.github.com>
2021-03-11 22:19:55 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick"> click it </button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'my first vue projekt'
+    }
+  },
+methods: {
+  handleClick() {  
+    console.log(this.$refs.name)
+
+  }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
